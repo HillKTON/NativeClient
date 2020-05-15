0bcc10439ca04ba50f65adad8b82bd8ba380a958
HillKTON <56476391+HillKTON@users.noreply.github.com>
2020-05-15 14:27:26 +0500

    v1.0

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import os
 import json
+from colorama import init as color_init
+from termcolor import colored
+from time import sleep
 
 base_folder = 'native_client'
+__version__ = '1.0 (15.05.2020)'
 
 
 def start():
-    print('Native client:')
-    setup()
+    os.system('cls')
+    color_init()
+    print(f'Версия: {__version__}')
+    description = 'Программа не для продажи...\n\n' \
+                  'NativeClient предназначен для быстрой установки и удаления чита NativeTrainer, для использования\n' \
+                  'переместите файлы чита в папку "native_client\\bin", а также укажите путь до папки игры в файле\n' \
+                  '"native_client\\settings.json", вместо "none". Путь должен быть в формате\n' \
+                  '"D:\\\\Steam\\\\steamapps\\\\common\\\\Grand Theft Auto V"\n'
+    print(colored(description, 'yellow'))
+    main()
 
 
 def setup():
+    os.system('cls')
     if os.path.isdir(base_folder) is False:
-        print('Производится установка')
+        print(colored('Производится установка', 'yellow'))
+        sleep(1)
         os.mkdir(base_folder)
         os.mkdir(f'{base_folder}\\bin')
         with open(f'{base_folder}\\settings.json', 'w') as io_file:
             settings_text = {'Game Folder': None}
             json.dump(settings_text, io_file, indent=2)
+        print(colored('Установка прошла успешно', 'yellow'))
+        sleep(3)
     else:
         pass
-    os.system('cls')
-    main()
+    start()
 
 
 def main():
@@ -29,44 +44,44 @@ def main():
             global game_folder
             game_folder = json.load(io_file)['Game Folder']
         if 'NativeTrainer.asi' in os.listdir(path=game_folder):
-            print('Трейнер уже установлен, хотите его откючить?\n'
-                  '[1] Да\n'
-                  '[2] Нет')
+            print(colored('Трейнер уже установлен, хотите его удалить?\n'
+                          '[1] Да\n'
+                          '[2] Нет', 'green'))
             while True:
-                q = input('>> ')
+                q = input('\n>> ')
                 if q in ['1', '2']:
                     if q == '1':
                         disable()
-                        print('Отключение прошло успешно!')
+                        print(colored('\nУдаление прошло успешно!', 'cyan'))
                         stop()
                     else:
                         stop()
                 else:
-                    print('Неизвесный индекс')
+                    print(colored('\nНеизвесный индекс', 'cyan'))
                     continue
         else:
-            print('Трейнер не установлен, хотите его установить?\n'
-                  '[1] Да\n'
-                  '[2] Нет')
+            print(colored('Трейнер не установлен, хотите его установить?\n'
+                          '[1] Да\n'
+                          '[2] Нет', 'cyan'))
             while True:
-                q = input('>> ')
+                q = input('\n>> ')
                 if q in ['1', '2']:
                     if q == '1':
                         enable()
-                        print('Установка прошла успешно!')
+                        print(colored('\nУстановка прошла успешно!', 'cyan'))
                         stop()
                     else:
                         stop()
                 else:
-                    print('Неизвесный индекс')
+                    print(colored('\nНеизвесный индекс', 'cyan'))
                     continue
     else:
-        print('Трейнер отсутствует в папке "bin"')
+        print(colored('\nТрейнер отсутствует в папке "bin"', 'cyan'))
         stop()
 
 
 def stop():
-    input('Нажмите Enter что-бы выйти...\n')
+    input(colored('Нажмите Enter что-бы выйти...\n', 'yellow'))
     quit()
 
 
@@ -88,4 +103,4 @@ def disable():
 
 
 if __name__ == '__main__':
-    start()
+    setup()
